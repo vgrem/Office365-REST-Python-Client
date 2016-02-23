d998c0358d7d419e01b51f5f55deb3a099f17417
Yenthe <yenthevg@gmail.com>
2016-02-23 14:39:05 +0100

    [Work in progress] Function for HTTPS POSTS
    Hi,
    
    This is a work in progress for enabling posts to Sharepoint online. When you want to checkout a document for example you will need a POST method, in place of a get.
    Current error:
    
    Traceback (most recent call last):
      File "connector.py", line 28, in <module>
        request = ClientRequest(url,ctxAuth)
      File "/home/administrator/Desktop/odoo_sp_connector/client/ClientRequest.py", line 20, in __init__
        self.DefaultHeadersHttps['Cookie'] = authContext.getAuthenticationCookie()
    AttributeError: 'ClientRequest' object has no attribute 'DefaultHeadersHttps'
    
    Any idea?

--- client/ClientRequest.py
+++ client/ClientRequest.py
@@ -12,6 +12,11 @@ class ClientRequest(object):
             'accept':'application/json;odata=verbose'
         }
         self.defaultHeaders['Cookie'] = authContext.getAuthenticationCookie()
+        self.defaultHeadersHttps = {
+            'content-type':'application/x-www-form-urlencoded',
+            'accept':'application/json;odata=verbose'
+        }
+        self.DefaultHeadersHttps['Cookie'] = authContext.getAuthenticationCookie()
         self.formDigestValue = None
         
     
@@ -33,6 +38,26 @@ class ClientRequest(object):
             return {}
         except requests.exceptions.RequestException as e:
             return "Error: {}".format(e)
+
+    def postQuery(self, requestUrl, headers={}, data={}):
+        "Execute client post - for example for checking out files"
+        try:
+            url = self.url + requestUrl
+            for key in self.defaultHeadersHttps:
+                headers[key] = self.defaultHeadersHttps[key]
+            if data or 'X-HTTPS-Method' in headers:
+                if not self.formDigestValue:
+                    self.requestFormDigest()
+                    headers['X-RequestDigest'] = self.formDigestValue
+                result = requests.post(url, headers=headers, json = data)
+            else:
+                result = requests.post(url=url, headers=headers)
+            if result.content:
+                return result.json()
+            return {}
+        except requests.exceptions.RequestException as e:
+            return "Error: {}".format(e)
+
 
     def requestFormDigest(self):
          "Request Form Digest"
