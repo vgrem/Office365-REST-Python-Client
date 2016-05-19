ff44f78de9d1050330ea3193a801a7ad68414288
vgrem <vvgrem@gmail.com>
2016-05-20 00:52:43 +0300

    Provided unit tests for List resource

--- tests/test_list.py
+++ tests/test_list.py
@@ -1,11 +1,31 @@
+from random import randint
+
 from tests.sharepoint_case import SPTestCase
 
 
 class TestList(SPTestCase):
-    def test_read_list(self):
-        list_title = "Tasks"
-        list_obj = self.context.web.lists.get_by_title(list_title)
-        ctx = list_obj.context
-        ctx.load(list_obj)
-        ctx.execute_query()
-        self.assertEqual(list_title, list_obj.properties['Title'])
+    list_title = "Tasks" + str(randint(0, 1000))
+
+    def test_1_create_list(self):
+        list_properties = {'__metadata': {'type': 'SP.List'}, 'AllowContentTypes': True, 'BaseTemplate': 171,
+                           'Title': self.list_title}
+        list_to_create = self.context.web.lists.add(list_properties)
+        self.context.execute_query()
+        self.assertEqual(list_properties['Title'], list_to_create.properties['Title'])
+
+    def test_2_read_list(self):
+        list_to_read = self.context.web.lists.get_by_title(self.list_title)
+        self.context.load(list_to_read)
+        self.context.execute_query()
+        self.assertEqual(self.list_title, list_to_read.properties['Title'])
+
+    def test_3_update_list(self):
+        list_to_update = self.context.web.lists.get_by_title(self.list_title)
+        list_properties = {'__metadata': {'type': 'SP.List'}, 'Title': self.list_title}
+        list_to_update.update(list_properties)
+        self.context.execute_query()
+
+    def test_4_delete_list(self):
+        list_to_delete = self.context.web.lists.get_by_title(self.list_title)
+        list_to_delete.delete_object()
+        self.context.execute_query()
