231ba9dd7f1f5372acdf229dd0b61fb0ab609ed6
Konrad Gądek <kgadek@griddynamics.com>
2017-02-14 16:02:33 +0100

    Packaging for PyPI
    Adds required metadata for the project. Closes #10.
    
    With these changes, creating package is performed by:
    
        python setup.py sdist
    
    To upload a package, first refer to the docs [1]. After initial setup:
    
        twine upload dist/*
    
    [1]: https://packaging.python.org/distributing/#uploading-your-project-to-pypi

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from distutils.core import setup
+import setuptools
+
+def read(fname):
+    """From an_example_pypi_project (https://pypi.python.org/pypi/an_example_pypi_project). """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="Office365-REST-Python-Client",
+    version="1.0.0"
+    author="Vadim Gremyachev",
+    author_email="vvgrem@gmail.com",
+    maintainer="Konrad Gądek",
+    maintainer_email="kgadek@gmail.com",
+    description="Office 365 REST client for Python",
+    long_description=read("README.md"),
+    url="https://github.com/vgrem/Office365-REST-Python-Client",
+    install_requires=['requests'],
+    license="MIT",
+    keywords="git",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries"
+    ],
+    packages=setuptools.find_packages()
+)
+
