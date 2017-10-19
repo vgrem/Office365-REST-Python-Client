b18b6526211505d93102587257b053edd0d4132c
Toilal <toilal.dev@gmail.com>
2017-10-19 09:41:42 +0200

    Added update and delete_object methods for ListItem

--- office365/sharepoint/listitem.py
+++ office365/sharepoint/listitem.py
@@ -1,5 +1,16 @@
+from office365.runtime.client_query import ClientQuery
 from office365.sharepoint.securable_object import SecurableObject
 
 
 class ListItem(SecurableObject):
     """ListItem client object resource"""
+
+    def update(self):
+        """Update the list."""
+        qry = ClientQuery.update_entry_query(self)
+        self.context.add_query(qry)
+
+    def delete_object(self):
+        """Deletes the list."""
+        qry = ClientQuery.delete_entry_query(self)
+        self.context.add_query(qry)
