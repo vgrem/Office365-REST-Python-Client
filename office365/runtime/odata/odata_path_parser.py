b35befc9677541295609f4e55eea6fc2c4d7ab08
Toilal <toilal.dev@gmail.com>
2017-10-26 09:49:14 +0200

    Add more OData parameter format escapes

--- office365/runtime/odata/odata_path_parser.py
+++ office365/runtime/odata/odata_path_parser.py
@@ -26,7 +26,19 @@ class ODataPathParser(object):
     @staticmethod
     def encode_method_value(value):
         if isinstance(value, basestring):
-            value = "'{0}'".format(value.replace("'", "''"))
+            value = value.replace("'", "''")
+
+            # Same replacements as SQL Server
+            # https://web.archive.org/web/20150101222238/http://msdn.microsoft.com/en-us/library/aa226544(SQL.80).aspx
+            # https://stackoverflow.com/questions/4229054/how-are-special-characters-handled-in-an-odata-query#answer-45883747
+            value = value.replace('%', '%25')
+            value = value.replace('+', '%2B')
+            value = value.replace('/', '%2F')
+            value = value.replace('?', '%3F')
+            value = value.replace('#', '%23')
+            value = value.replace('&', '%26')
+
+            value = "'{0}'".format(value)
         elif isinstance(value, bool):
             value = str(value).lower()
         return value
