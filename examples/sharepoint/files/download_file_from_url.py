f377ebe95ec22fa7c8a6ea70c52f12004c6d5556
Maksim Beliaev <beliaev.m.s@gmail.com>
2020-07-31 12:04:34 +0200

    1. updated example on download file

--- examples/sharepoint/files/download_file_from_url.py
+++ examples/sharepoint/files/download_file_from_url.py
@@ -1,23 +1,34 @@
 import os
 import tempfile
 
-from office365.sharepoint.client_context import ClientContext
-from office365.runtime.auth.UserCredential import UserCredential
-
-ctx = ClientContext(site_url).with_credentials(UserCredential(username, password))
-
 from office365.runtime.auth.user_credential import UserCredential
 from office365.sharepoint.files.file import File
-print(dir(UserCredential))
-# from settings import settings
-#
-# abs_file_url = "{site_url}sites/team/Shared Documents/sample.docx".format(site_url=settings.get('url'))
-# user_credentials = UserCredential(settings.get('user_credentials').get('username'),
-#                                   settings.get('user_credentials').get('password'))
-#
-#
-# file_name = os.path.basename(abs_file_url)
-# with tempfile.TemporaryDirectory() as local_path:
-#     with open(os.path.join(local_path, file_name), 'wb') as local_file:
-#         file = File.from_url(abs_file_url).with_credentials(user_credentials).download(local_file).execute_query()
-#     print("'{0}' file has been downloaded into {1}".format(file.serverRelativeUrl, local_file.name))
+from settings import settings
+
+# where settings looks like
+# settings = {
+#     "user_credentials": {
+#         "username": "username",
+#         "password": "password"
+#     },
+#     "url": "https://company.sharepoint.com/",
+#     "team": "my-team",
+#     "file_path": "General/resolve_IP.bat"
+# }
+
+
+abs_file_url = "{site_url}sites/{team}/{file_path}".format(
+                                                               site_url=settings.get('url'),
+                                                               team=settings.get('team'),
+                                                               file_path=settings.get('file_path')
+                                                           )
+
+user_credentials = UserCredential(settings.get('user_credentials').get('username'),
+                                  settings.get('user_credentials').get('password'))
+
+
+file_name = os.path.basename(abs_file_url)
+with tempfile.TemporaryDirectory() as local_path:
+    with open(os.path.join(local_path, file_name), 'wb') as local_file:
+        file = File.from_url(abs_file_url).with_credentials(user_credentials).download(local_file).execute_query()
+    print("'{0}' file has been downloaded into {1}".format(file.serverRelativeUrl, local_file.name))
