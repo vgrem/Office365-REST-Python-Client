bc71fab073934027656517c8678a6aed332d1d43
vgrem <vvgrem@gmail.com>
2022-01-23 13:17:02 +0200

    OneDrive examples: download a large file

---
+++ examples/onedrive/download_file_default.py
@@ -0,0 +1,16 @@
+import os
+import tempfile
+
+from examples import acquire_token_by_username_password
+from office365.graph_client import GraphClient
+from office365.onedrive.driveitems.driveItem import DriveItem
+
+client = GraphClient(acquire_token_by_username_password)
+# 1. address file by path and get file metadata
+file_item = client.me.drive.root.get_by_path("archive/Sample.rtf").get().execute_query()  # type: DriveItem
+
+# 2. download file content
+with tempfile.TemporaryDirectory() as local_path:
+    with open(os.path.join(local_path, file_item.name), 'wb') as local_file:
+        file_item.download(local_file).execute_query()
+    print("File '{0}' has been downloaded into {1}".format(file_item.name, local_file.name))
---
+++ examples/onedrive/download_large_file.py
@@ -0,0 +1,20 @@
+import os
+import tempfile
+
+from examples import acquire_token_by_username_password
+from office365.graph_client import GraphClient
+from office365.onedrive.driveitems.driveItem import DriveItem
+
+
+def print_download_progress(offset):
+    print("Downloaded '{0}' bytes...".format(offset))
+
+
+client = GraphClient(acquire_token_by_username_password)
+# # 1. address file by path and get file metadata
+file_item = client.me.drive.root.get_by_path("archive/big_buck_bunny.mp4").get().execute_query()  # type: DriveItem
+# 2 download a large file (chunked file download)
+with tempfile.TemporaryDirectory() as local_path:
+    with open(os.path.join(local_path, file_item.name), 'wb') as local_file:
+        file_item.download_session(local_file, print_download_progress).execute_query()
+    print("File '{0}' has been downloaded into {1}".format(file_item.name, local_file.name))
