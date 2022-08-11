fa4186a60614f7e9e47dec3be6b2bf55157a2c5b
vgrem <vvgrem@gmail.com>
2022-08-11 18:16:40 +0300

    Todo API: TaskList serialize fix

--- office365/todo/task_list.py
+++ office365/todo/task_list.py
@@ -1,16 +1,12 @@
 from office365.directory.extensions.extension import Extension
 from office365.entity import Entity
 from office365.entity_collection import EntityCollection
-from office365.runtime.odata.v4.json_format import V4JsonFormat
 from office365.runtime.paths.resource_path import ResourcePath
 from office365.todo.task import TodoTask
 
 
 class TodoTaskList(Entity):
     """A list in Microsoft To Do that contains one or more todoTask resources."""
-
-    def to_json(self, json_format=None):
-        return super(TodoTaskList, self).to_json(V4JsonFormat.no_metadata())
 
     @property
     def extensions(self):
@@ -28,4 +24,4 @@ class TodoTaskList(Entity):
 
     @property
     def entity_type_name(self):
-        return "microsoft.graph.todoTaskList"
+        return None
