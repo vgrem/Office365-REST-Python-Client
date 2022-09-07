a80ad40fc22f923ef47e9525b9730a09a11d24c3
vgrem <vvgrem@gmail.com>
2022-09-07 23:30:15 +0300

    2.3.14 release

--- examples/sharepoint/lists/read_list_via_query.py
+++ examples/sharepoint/lists/read_list_via_query.py
@@ -12,17 +12,22 @@ def create_paged_query(page_size):
     qry = CamlQuery()
     qry.ViewXml = f"""
     <View Scope='RecursiveAll'>
-       <Query></Query>
+       <Query>
+           <Where><Geq><FieldRef Name='Created'/><Value Type='DateTime' IncludeTimeValue='False'>2020-05-10T18:59:10Z</Value></Geq></Where>
+       </Query>
+       <ViewFields>
+            <FieldRef Name='Title' />
+       </ViewFields>
        <RowLimit Paged='TRUE'>{page_size}</RowLimit>
    </View>
    """
     return qry
 
-
+# 2020-03-20T14:30:43
 ctx = ClientContext(test_team_site_url).with_credentials(test_client_credentials)
 target_list = ctx.web.lists.get_by_title("Contacts_Large")
 list_qry = create_paged_query(50)
 items = target_list.get_items(list_qry).execute_query()
 print("Loaded items count: {0}".format(len(items)))
 for index, item in enumerate(items):  # type: int, ListItem
-    print("{0}: {1}".format(index, item.properties['Title']))
+    print("{0}: {1}".format(index, item.properties['Created']))
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with io.open("README.md", mode='r', encoding='utf-8') as fh:
 
 setup(
     name="Office365-REST-Python-Client",
-    version="2.3.13",
+    version="2.3.14",
     author="Vadim Gremyachev",
     author_email="vvgrem@gmail.com",
     maintainer="Konrad Gądek, Domenico Di Nicola",
