e4cacb53df4d63cc588fd42458750713abec3e3b
vgrem <vvgrem@gmail.com>
2022-09-28 22:17:45 +0300

    #559: SharePoint API: filter list items example

--- examples/sharepoint/lists/read_list_via_query.py
+++ examples/sharepoint/lists/read_list_via_query.py
@@ -1,11 +1,15 @@
+import datetime
+
 from office365.sharepoint.client_context import ClientContext
 from office365.sharepoint.listitems.caml.query import CamlQuery
 from office365.sharepoint.listitems.listitem import ListItem
 from tests import test_client_credentials, test_team_site_url
 
 
-def create_paged_query(page_size):
+def query_by_datetime(source_list, from_datetime, page_size=100):
     """
+    :type source_list: office365.sharepoint.lists.list.List
+    :type from_datetime: datetime.datetime
     :type page_size: int
     """
 
@@ -13,21 +17,34 @@ def create_paged_query(page_size):
     qry.ViewXml = f"""
     <View Scope='RecursiveAll'>
        <Query>
-           <Where><Geq><FieldRef Name='Created'/><Value Type='DateTime' IncludeTimeValue='False'>2020-05-10T18:59:10Z</Value></Geq></Where>
+           <Where>
+              <Gt>
+                 <FieldRef Name='Created'/>
+                 <Value Type='DateTime' IncludeTimeValue='True'>{from_datetime.isoformat()}</Value>
+              </Gt>
+           </Where>
        </Query>
-       <ViewFields>
-            <FieldRef Name='Title' />
-       </ViewFields>
        <RowLimit Paged='TRUE'>{page_size}</RowLimit>
-   </View>
-   """
-    return qry
+    </View>
+    """
+    return source_list.get_items(qry)
 
-# 2020-03-20T14:30:43
+
+def query_by_datetime_alt(source_list, from_datetime):
+    """
+    :type source_list: office365.sharepoint.lists.list.List
+    :type from_datetime: datetime.datetime
+    """
+
+    filter_text = "Created gt datetime'{0}'".format(from_datetime.isoformat())
+    return source_list.items.filter(filter_text).get()
+
+
 ctx = ClientContext(test_team_site_url).with_credentials(test_client_credentials)
-target_list = ctx.web.lists.get_by_title("Contacts_Large")
-list_qry = create_paged_query(50)
-items = target_list.get_items(list_qry).execute_query()
+list_title = "Site Pages"
+site_pages = ctx.web.lists.get_by_title(list_title)
+from_dt = datetime.datetime(2022, 1, 20, 0, 0)
+items = query_by_datetime(site_pages, from_dt).execute_query()
 print("Loaded items count: {0}".format(len(items)))
 for index, item in enumerate(items):  # type: int, ListItem
     print("{0}: {1}".format(index, item.properties['Created']))
