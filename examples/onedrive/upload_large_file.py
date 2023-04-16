1c73ea97baef7a0c1618c0aa80f8f93a5daeeeb5
vgrem <vvgrem@gmail.com>
2023-04-16 11:52:40 +0300

    UploadSession type fix

--- examples/onedrive/upload_large_file.py
+++ examples/onedrive/upload_large_file.py
@@ -3,16 +3,15 @@ from office365.graph_client import GraphClient
 
 client = GraphClient(acquire_token_by_username_password)
 
-chunk_size = 1 * 1024 * 1024
+chunk_size = 3 * 1024 * 1024
 
 
 def print_progress(range_pos):
     print("{0} bytes uploaded".format(range_pos))
 
 
-
 local_path = "../../tests/data/big_buck_bunny.mp4"
 remote_folder = client.me.drive.root.get_by_path("archive")
 remote_file = remote_folder.resumable_upload(local_path, chunk_size=chunk_size,
-                                                 chunk_uploaded=print_progress).get().execute_query()
+                                             chunk_uploaded=print_progress).get().execute_query()
 print(f"File {remote_file.web_url} has been uploaded")
