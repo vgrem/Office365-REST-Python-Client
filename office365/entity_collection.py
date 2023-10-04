03019ec52d819e567227c5ec2fb7cadf491b7b09
Johann Kellerman <kellerza@gmail.com>
2023-10-04 14:09:48 +0200

    ClientContext

--- office365/entity_collection.py
+++ office365/entity_collection.py
@@ -1,11 +1,13 @@
-from typing import TypeVar
+from typing import TYPE_CHECKING, TypeVar, Any
 
-from office365.graph_client import GraphClient
 from office365.runtime.client_object_collection import ClientObjectCollection
 from office365.runtime.compat import is_string_type
 from office365.runtime.paths.item import ItemPath
 from office365.runtime.paths.resource_path import ResourcePath
 from office365.runtime.queries.create_entity import CreateEntityQuery
+
+if TYPE_CHECKING:
+    from office365.graph_client import GraphClient
 
 T = TypeVar("T")
 
