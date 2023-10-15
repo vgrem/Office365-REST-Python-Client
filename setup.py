c6697421c5150f658741798b8d6a2ef76543ca57
vgrem <vvgrem@gmail.com>
2023-10-15 20:43:14 +0300

    2.5.0 release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with io.open("README.md", mode="r", encoding="utf-8") as fh:
 
 setup(
     name="Office365-REST-Python-Client",
-    version="2.4.4",
+    version="2.5.0",
     author="Vadim Gremyachev",
     author_email="vvgrem@gmail.com",
     maintainer="Konrad Gądek, Domenico Di Nicola",
