337a6976cb4543d6d265566f461da3d0d11890b6
vgrem <vvgrem@gmail.com>
2024-04-11 19:14:15 +0300

    examples for working with delegated & application permissions

---
+++ examples/directory/applications/revoke_application_perms.py
@@ -0,0 +1,27 @@
+"""
+Revoke an app role assignment from a client service principal
+
+https://learn.microsoft.com/en-us/graph/permissions-grant-via-msgraph?tabs=http&pivots=grant-application-permissions
+"""
+
+from office365.graph_client import GraphClient
+from tests import (
+    test_admin_principal_name,
+    test_client_id,
+    test_password,
+    test_tenant,
+    test_username,
+)
+
+# client = GraphClient.with_token_interactive(
+#    test_tenant, test_client_id, test_admin_principal_name
+# )
+
+client = GraphClient.with_username_and_password(
+    test_tenant, test_client_id, test_username, test_password
+)
+
+
+# Get resource
+resource = client.service_principals.get_by_name("Microsoft Graph")
+resource.revoke_application(test_client_id, "MailboxSettings.Read").execute_query()
