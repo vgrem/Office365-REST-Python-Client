d1d6df29d47942a297721b242a2e68b085d2ce81
vgrem <vvgrem@gmail.com>
2024-07-25 21:10:28 +0300

    examples: taxonomy get value, model updates

--- examples/sharepoint/files/move_file.py
+++ examples/sharepoint/files/move_file.py
@@ -12,10 +12,11 @@ ctx = ClientContext(test_team_site_url).with_credentials(test_user_credentials)
 file_from = ctx.web.get_file_by_server_relative_path(
     "Shared Documents/Financial Sample.xlsx"
 )
+
 # folder_to = ctx.web.get_folder_by_server_relative_url("Shared Documents")
-folder_to = "Shared Documents"
+folder_to = "Shared Documents/Archive"
 
 file_to = file_from.move_to_using_path(
     folder_to, MoveOperations.overwrite
 ).execute_query()
-print("'{0}' moved into '{1}'".format(file_from, file_to))
+print("'{0}' moved into '{1}'".format(file_from, file_to.server_relative_path))
---
+++ examples/sharepoint/taxonomy/get_field_value.py
@@ -0,0 +1,18 @@
+"""
+Demonstrates how to get taxonomy field value
+"""
+import sys
+
+from office365.sharepoint.client_context import ClientContext
+from tests import test_client_credentials, test_team_site_url
+
+ctx = ClientContext(test_team_site_url).with_credentials(test_client_credentials)
+custom_list = ctx.web.lists.get_by_title("Requests")
+list_items = custom_list.items.get().execute_query()
+if len(list_items) == 0:
+    sys.exit("No list items were found.")
+
+tax_field_value = list_items[0].get_property("Country")
+tax_field_multi_value = list_items[0].get_property("Countries")
+print(tax_field_value['TermGuid'])
+print(tax_field_multi_value[0]['TermGuid'])
